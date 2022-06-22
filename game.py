2d2baaec95f58fd8ce0ecb8f51f11b4a005aa972
01110010pl <01110010pl@gmail.com>
2022-06-22 14:13:00 +0200

    Create the project; create game.py and main.py with first classes

---
+++ game.py
@@ -0,0 +1,57 @@
+import random
+
+class TicTacToe:
+    """Tic Tac Toe game's class."""
+    
+    def __init__(self):
+        """Init of Tic Tac Toe game's object."""
+        self._board = [i for i in range(9)]
+        self._avaiablePlayers = ["X", "Y"]
+        self._currentTurn = 0
+    
+    @property
+    def board(self):
+        return self._board[:]
+    
+    @property
+    def currentTurn(self):
+        return self._currentTurn
+    
+    def choicePlayerTurn(self):
+        """This method sets the first turn of the game."""
+        self._currentTurn = random.choice(self._avaiablePlayers)
+    
+    def changeTurn(self):
+        """Change the current turn of players."""
+        if self._currentTurn == self._avaiablePlayers[0]:
+            self._currentTurn = self._avaiablePlayers[1]
+        else:
+            self._currentTurn = self._avaiablePlayers[0]
+
+    def setPlace(self, place):
+        """Method gets from players, a place where they want to set; if 
+        the place is avaiable, method return True; against it's return False"""
+        if self._board[place] == place:
+            self._board[place] = self._currentTurn
+            return True
+        else:
+            return False
+    
+    def checkWin(self):
+        """Method returns True, if some user win the game; return False if not."""
+        first = self._board[0] == self._board[1] == self._board[2]
+        second = self._board[3] == self._board[4] == self._board[5]
+        third = self._board[6] == self._board[7] == self._board[8]
+        if first or second or third:
+            return True
+        first = self._board[0] == self._board[3] == self._board[6]
+        second = self._board[1] == self._board[4] == self._board[7]
+        third = self._board[2] == self._board[5] == self._board[8]
+        if first or second or third:
+            return True
+        first = self._board[0] == self._board[4] == self._board[8]
+        second = self._board[2] == self._board[4] == self._board[6]
+        if first or second:
+            return True
+        
+        return False
---
+++ main.py
@@ -0,0 +1,10 @@
+from game import TicTacToe
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+else:
+    print("It's not a module! Please run this file like a normal program!")
+    input("Press any key to end...")
